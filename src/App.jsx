b72ad507547fed582921eab96fef0b6f8a83140c
Sandip-Chavda <chavdas766@gmail.com>
2024-12-23 14:52:23 +0530

    Project Setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <main>
+      <h1 className="text-5xl text-violet-300 font-zentry">
+        Welcome to Zentry Clone
+      </h1>
+    </main>
+  );
+};
+
+export default App;
